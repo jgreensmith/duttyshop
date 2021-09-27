33ebfd5bf115a283d6d667317606b642c40d321c
James Greensmith <jgreensmith13@gmail.com>
2021-09-27 20:13:16 +0100

    get data from commerce

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import { StoreProvider } from '../components/Store';
+import '../styles/globals.css';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+import 'nprogress/nprogress.css';
+import { useEffect } from 'react';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    //remove server side injected css
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.getInitialProps = async () => {
+  return {
+    pageProps: {
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  };
+};
